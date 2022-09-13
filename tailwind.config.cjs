efaecb8a23faaefe7a22812e6e8bbe856847f808
nafees nazik <nafeesnazik21@gmail.com>
2022-09-13 23:34:28 +0530

    chore: add tailwind css and configs

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./public/**/*.html", "./src/**/*.{astro,js,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
